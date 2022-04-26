9062141d4919d81d345915143f1b5263217c0b2a
michellepi <57605186+michellepi@users.noreply.github.com>
2022-04-26 18:38:32 +0100

    New high lowpass filtering and plot PSD

--- Thea code/label_recordings.m
+++ Thea code/label_recordings.m
@@ -3,14 +3,15 @@ clearvars
 disp_ECG = 1
 
 label_data = 0
+root = "../../Healthy Volunteer/EMILIE - SITTING";
 
-PCG_dir = "../../EKO/Patient 2/PCG";
+PCG_dir = fullfile(root, "/PCG")
 PCG_files = dir(fullfile(PCG_dir, '**', '*.wav'));
 [~,p_ind]=sort({PCG_files.name});
 PCG_files = PCG_files(p_ind);
 
 if disp_ECG 
-    ECG_dir = "../../EKO/Patient 2/ECG";
+    ECG_dir = fullfile(root, "/ECG")
     ECG_files = dir(fullfile(ECG_dir,'**', '*.wav'));
     [~,e_ind] = sort({ECG_files.name});
     ECG_files = ECG_files(e_ind);
@@ -32,12 +33,24 @@ S2 = {};
 
 %% run through recordings for manual annotation
 for i=1:length(PCG_paths)
-    figure();
     disp(PCG_files(i).name);
     resample_rate = 1000;
     % read audio file 
     disp(PCG_paths(i))
     [p_signal, p_fs] = audioread(PCG_paths(i));
+
+    M=p_fs;% define the window-length
+    window=hanning(M); % define the window to use (Hanning)
+    overlap=0.5; % define the overlap for windows
+    [P,f]=spec2(p_signal,window,overlap,p_fs);% calculate the PSD
+    figure();
+    plot(f,P);% plot the result
+    xticks(0:100:max(f));
+    xlabel('frequency (Hz)');% label axes
+    ylabel('PSD (microV^2/Hz)');
+    p_title = strcat('PSD of PCG signal ', PCG_files(i).name)
+
+    title(p_title);
 
     % re-sample file
     p_signal = resample(p_signal, resample_rate, p_fs);
@@ -45,7 +58,21 @@ for i=1:length(PCG_paths)
     t = 0:dt:(length(p_signal)*dt)-dt;
 
     % de-noise signal
-    p_signal = applyButterworthBandpassFilter(25, 165, 3, resample_rate, p_signal);
+    %p_signal = applyButterworthBandpassFilter(25, 165, 3, resample_rate, p_signal);
+     p_low={};
+     p_low.N=2;
+     p_low.type='butter';
+     p_low.lphp='low';
+     [b,a]=make_digital_filter(150, p_fs, p_low);
+     p_signal = filtfilt(b, a, p_signal);
+
+     p_high={};
+     p_high.N=2;
+     p_high.type='butter';
+     p_high.lphp='high';
+     [b,a]=make_digital_filter(25, p_fs, p_high);
+     p_signal = filtfilt(b, a, p_signal);
+
     p_signal = p_signal./max(abs(p_signal));
     
     % display hilbert envelope to see if there are distinct peaks in env
@@ -53,10 +80,37 @@ for i=1:length(PCG_paths)
      
     if disp_ECG
         [e_signal, e_fs] = audioread(ECG_paths(i));
+        disp(e_fs)
         e_dt = 1/e_fs; 
         e_t = 0:e_dt:(length(e_signal)*e_dt)-e_dt;
+
+        M=e_fs;% define the window-length
+        window=hanning(M); % define the window to use (Hanning)
+        overlap=0.5; % define the overlap for windows
+        [P,f]=spec2(e_signal,window,overlap,e_fs);% calculate the PSD
+        figure();
+        plot(f,P);% plot the result
+        xticks(0:10:max(f));
+        xlabel('frequency (Hz)');% label axes
+        ylabel('PSD (microV^2/Hz)');
+        p_title = strcat('PSD of ECG signal ', PCG_files(i).name)
+        title(p_title);
+       
+        p_low={};
+        p_low.N=2;
+        p_low.type='butter';
+        p_low.lphp='low';
+        [b,a]=make_digital_filter(150, e_fs, p_low);
+        e_signal = filtfilt(b, a, e_signal)
         
-        e_signal = applyButterworthBandpassFilter(1.5, 100, 4, e_fs, e_signal);
+        p_high={};
+        p_high.N=2;
+        p_high.type='butter';
+        p_high.lphp='high';
+        [b,a]=make_digital_filter(1, e_fs, p_high);
+        e_signal = filtfilt(b, a, e_signal);
+        
+        %e_signal = applyButterworthBandpassFilter(1, 20, 4, e_fs, e_signal);
         
         wo = 50/(e_fs/2);  
         bw = wo;
@@ -67,10 +121,12 @@ for i=1:length(PCG_paths)
         kernel = ones(windowWidth,1) / windowWidth;
         e_signal = filtfilt(kernel, 1, e_signal);
         
+        
         e_signal = e_signal./max(abs(e_signal));
-        %e_signal = -e_signal;
+        e_signal = -e_signal; % Flip signal
 
         % display cleaned signal 
+        figure();
         fig = tiledlayout(3,1);
         fig.TileSpacing = 'compact';
         fig.Padding = 'compact';
